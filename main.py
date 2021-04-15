e8fb91c8c4dd0a8e684b8d43417d8cde9a5eafd2
Christian <christgik12@gmail.com>
2021-04-15 21:52:07 +0200

    version 0.5

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
+agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+        'Chrome/61.0.3163.100 Safari/537.36'
 
-header={'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.1"
-                      "34 Safari/537.36"}
-
-
-def imgsearch(search, result_number=1):
-    query = search.replace(' ', '+')
-    result = result_number
-    url = f"https://google.com/search?q={query}&source=lnms&tbm=isch"
-    print(url)
-    resp = requests.get(url, headers=header)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    print(soup)
+header = {'User-Agent': agent}
 
 
+def bing(query):
+    query = query.replace(" ", "%20")
+    url = f"https://www.bing.com/images/search?q={query}&form=HDRSC2&first=1&tsc=ImageBasicHover"
+    rsp = requests.get(url, headers=header)
+    soup = BeautifulSoup(rsp.text, "html.parser")
+    a_balise = soup.find_all("a", {"class": "iusc"})
+    bing = "https://www.bing.com"
+    final_link = []
+    for i in range(15):
+        href = f"{a_balise[i]}"
+        link_start = href.index("href=") + 6
+        link_end = href.index("m='") - 3
+        link = f"{bing}{href[link_start:link_end]}".replace("amp;", "")
+        final_link.append(link)
+    return "\n".join(final_link)
 
-imgsearch("usa", 10)
+
+
+
+
+print(bing("france"))
--- test.py
+++ test.py
@@ -1,11 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
+agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+        'Chrome/61.0.3163.100 Safari/537.36'
+header = {'User-Agent': agent}
 
-recherche = input("Ta recherche : ")
-recherche = recherche.replace(" ", "%20")
-url = f"https://unsplash.com/s/photos/{recherche}"
-print(url)
-resp = requests.get(url)
-soup = BeautifulSoup(resp.content, "html.parser")
-print(soup)
+
+def img_fin(link):
+    img_req = requests.get(url=link)
+    img_pars = BeautifulSoup(img_req.text, "html.parser")
+    print(img_pars)
+
+
+url_image = "https://www.bing.com/images/search?view=detailV2&ccid=s%2bOYui1H&id=2BFF496C57BF963630DB68300AE193053C8" \
+            "C12F5&thid=OIP.s-OYui1H2TGvEPfuAl-QfQHaKX&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fRb3e398ba2d47d" \
+            "931af10f7ee025f907d%3frik%3d9RKMPAWT4QowaA%26riu%3dhttp%253a%252f%252fsparkhistory.com%252fwp-content%2" \
+            "52fuploads%252f2018%252f04%252fparis-france-eiffel-tower-roadview-SparkHistory.jpg%26ehk%3dc9O261A3tce" \
+            "p3IivpPVLOBxOdhW4fE4W3yhSMZR4Jxw%253d%26risl%3d%26pid%3dImgRaw&exph=3673&expw=2624&q=france&simid=6080" \
+            "36445704451443&ck=D4DE5E1E599D3E0A37B1F8B442867B81&selectedIndex=0&FORM=IRPRS"
+
+print(url_image)
+
+
+url = "https://www.bing.com/images/search?q=france&form=HDRSC2&first=1&tsc=ImageBasicHover"
+rsp = requests.get(url, headers=header)
+soup = BeautifulSoup(rsp.text, "html.parser")
+a_balise = soup.find_all("a", {"class": "iusc"})
+link = []
+bing = "https://www.bing.com"
+
+for i in range(15):
+    href = f"{a_balise[i]}"
+    link_start = href.index("href=")+6
+    link_end = href.index("m='")-3
+    link = f"{bing}{href[link_start:link_end]}".replace("amp;", "")
+    print(link)
